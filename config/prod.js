f6ed496590a735c3c9911c29950828afbe50bbfd
James Langford <derivative7117@live.com>
2017-08-31 17:26:01 -0600

    updated prod.js

--- config/prod.js
+++ config/prod.js
@@ -3,6 +3,9 @@ module.exports = {
     MASSIVE_URI: process.env.MASSIVE_URI,
     SESSION_SECRET: process.env.SESSION_SECRET,
     secretKey: process.env.SECRET_KEY,
+    CLIENT_ID: process.env.CLIENT_ID,
+    CLIENT_SECRET: process.env.CLIENT_SECRET,
+    access_token: process.env.access_token,
     redirect_uri: `https://vimeo-clone.herokuapp.com/api/callback`,
     redirect_URL: `https://vimeo-clone.herokuapp.com/`
 }
