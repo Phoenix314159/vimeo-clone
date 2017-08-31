8e65445cdd13339a49d4e1ab2f20f8d6b7b46fd1
James Langford <derivative7117@live.com>
2017-08-31 17:40:00 -0600

    cleanup

--- server.js
+++ server.js
@@ -7,22 +7,22 @@ require('./services/middleware')(app);
 require('./routes/get')(app);
 require('./routes/post')(app);
 
-// fileArr.files.map(file => {
-//     app.use(express.static(__dirname + file));
-// })
+fileArr.files.map(file => {
+    app.use(express.static(__dirname + file));
+})
 
 //<--------- production ----------->
-process.env.PWD = process.cwd();
-
-app.use('/', express.static(process.env.PWD + '/dist'));
-
-fileArr.arr.filter(a => {
-    if(a !== fileArr.arr[0]){
-        return true;
-    }
-}).map(file => {
-    app.use(express.static(process.env.PWD + file));
-})
+// process.env.PWD = process.cwd();
+//
+// app.use('/', express.static(process.env.PWD + '/dist'));
+//
+// fileArr.arr.filter(a => {
+//     if(a !== fileArr.arr[0]){
+//         return true;
+//     }
+// }).map(file => {
+//     app.use(express.static(process.env.PWD + file));
+// })
 //<--------- production ----------->
 
 app.listen(config.port, () => {
