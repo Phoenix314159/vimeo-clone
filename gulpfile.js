e23debe35490c896507e459c9b717adb1c1603eb
James Langford <derivative7117@live.com>
2017-12-02 17:04:19 -0700

    updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,58 +1,58 @@
 const gulp = require('gulp'),
-    $ = require('gulp-load-plugins')({
-        pattern: ['gulp-*'],
-        replaceString: /\bgulp[\-.]/,
-        lazy: true,
-        camelize: true
-    }),
-    cachebust = new $.cachebust();
-
+  {htmlmin, sourcemaps: {init, write}, sass, concat, cssmin,
+    print, babel, ngAnnotate, uglify, cachebust} = require('gulp-load-plugins')({
+    pattern: ['gulp-*'],
+    replaceString: /\bgulp[\-.]/,
+    lazy: true,
+    camelize: true
+  }),
+  cacheBust = new cachebust()
 
 gulp.task('views', () => {
-    return gulp.src('./public/views/**/*')
-        .pipe($.htmlmin({collapseWhitespace: true}))
-        .pipe(gulp.dest('dist/views'))
-});
+  return gulp.src('./public/views/**/*')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('dist/views'))
+})
 
 gulp.task('build-css', () => {
-    return gulp.src('./public/css/**/*')
-        .pipe($.sourcemaps.init())
-        .pipe($.sass())
-        .pipe(cachebust.resources())
-        .pipe($.concat('styles.css'))
-        .pipe($.sourcemaps.write('./maps'))
-        .pipe($.cssmin())
-        .pipe(gulp.dest('dist/css'));
-});
+  return gulp.src('./public/css/**/*')
+    .pipe(init())
+    .pipe(sass())
+    .pipe(cacheBust.resources())
+    .pipe(concat('styles.css'))
+    .pipe(write('./maps'))
+    .pipe(cssmin())
+    .pipe(gulp.dest('dist/css'))
+})
 
 gulp.task('build-js', () => {
-    return gulp.src('./public/JS/**/*.js')
-        .pipe($.sourcemaps.init())
-        .pipe($.print())
-        .pipe($.babel({presets: ['es2015']}))
-        .pipe($.concat('bundle.js'))
-        .pipe($.ngAnnotate())
-        .pipe($.uglify())
-        .pipe($.sourcemaps.write('./'))
-        .pipe(gulp.dest('dist/js'));
-});
+  return gulp.src('./public/JS/**/*.js')
+    .pipe(init())
+    .pipe(print())
+    .pipe(babel({presets: ['es2015']}))
+    .pipe(concat('bundle.js'))
+    .pipe(ngAnnotate())
+    .pipe(uglify())
+    .pipe(write('./'))
+    .pipe(gulp.dest('dist/js'))
+})
 
 gulp.task('images', () => {
-    return gulp.src('./public/images/**/*')
-        .pipe(gulp.dest('dist/images'));
-});
+  return gulp.src('./public/images/**/*')
+    .pipe(gulp.dest('dist/images'))
+})
 
 gulp.task('build', ['views', 'build-css', 'build-js', 'images'], () => {
-    return gulp.src('./public/index.html')
-        .pipe(cachebust.references())
-        .pipe($.htmlmin({collapseWhitespace: true}))
-        .pipe(gulp.dest('dist'));
-});
+  return gulp.src('./public/index.html')
+    .pipe(cacheBust.references())
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('dist'))
+})
 
 gulp.task('watch', () => {
-    return gulp.watch(['./public/index.html', './public/css/**/*', './public/js/**/*', './public/views/**/*'], ['build']);
-});
+  return gulp.watch(['./public/index.html', './public/css/**/*', './public/js/**/*', './public/views/**/*'], ['build'])
+})
 
-gulp.task('default' , ['build', 'watch']);
+gulp.task('default', ['build', 'watch'])
 
 
--- services/middleware.js
+++ services/middleware.js
@@ -1,16 +1,16 @@
 const session = require('express-session'),
-    config = require('../config/config'),
+  {redirect_URL, SESSION_SECRET} = require('../config/config'),
     cors = require('cors'),
     bodyParser = require('body-parser'),
     corsOptions = {
-        origin: config.redirect_URL
+        origin: redirect_URL
     };
 
 module.exports = app => {
     app.use(bodyParser.json());
     app.use(cors(corsOptions));
     app.use(session({
-        secret: config.SESSION_SECRET,
+        secret: SESSION_SECRET,
         resave: false,
         saveUninitialized: true,
         rolling: true
