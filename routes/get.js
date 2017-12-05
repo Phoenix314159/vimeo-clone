84c0259f641cdda8f3af759ba16cff4065426456
James Langford <derivative7117@live.com>
2017-12-04 18:54:41 -0700

    object destructuring

--- routes/get.js
+++ routes/get.js
@@ -1,17 +1,19 @@
-const mainCtrl = require('../controllers/mainCtrl'),
-    vimeoCtrl = require('../controllers/vimeoCtrl');
+const {login, callback, logout, getAccessToken, getUser,
+    displayWatchLater, usersVideos} = require('../controllers/mainCtrl'),
+  {getVideoById, getVideos, getVideoByChannels,
+    getComments, getVideoByChannels} = require('../controllers/vimeoCtrl');
 
 module.exports = app => {
-    app.get('/api/videos', vimeoCtrl.getVideoById);
-    app.get('/api/videos/:pageNum', vimeoCtrl.getVideos);
-    app.get('/api/videos/channels/:channel', vimeoCtrl.getVideoByChannels);
-    app.get('/api/videos/:id/comments', vimeoCtrl.getComments);
-    app.get('/api/videos/channels/:channel', vimeoCtrl.getVideoByChannels);
-    app.get('/api/login', mainCtrl.login);
-    app.get('/api/callback', mainCtrl.callback);
-    app.get('/api/logout', mainCtrl.logout);
-    app.get('/api/accesstoken', mainCtrl.getAccessToken);
-    app.get('/api/currentuser', mainCtrl.getUser);
-    app.get('/api/usersvideos', mainCtrl.displayWatchLater);
-    app.get('/api/usersvideos', mainCtrl.usersVideos);
+    app.get('/api/videos', getVideoById);
+    app.get('/api/videos/:pageNum', getVideos);
+    app.get('/api/videos/channels/:channel', getVideoByChannels);
+    app.get('/api/videos/:id/comments', getComments);
+    app.get('/api/videos/channels/:channel', getVideoByChannels);
+    app.get('/api/login', login);
+    app.get('/api/callback', callback);
+    app.get('/api/logout', logout);
+    app.get('/api/accesstoken', getAccessToken);
+    app.get('/api/currentuser', getUser);
+    app.get('/api/usersvideos', displayWatchLater);
+    app.get('/api/usersvideos', usersVideos);
 }
